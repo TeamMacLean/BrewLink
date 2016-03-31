abf1685795085d69b15c074d6da9b202f41f2d82
Martin Page <wookoouk@gmail.com>
2016-03-31 10:47:13 +0100

    test

--- brewlink.go
+++ brewlink.go
@@ -90,7 +90,7 @@ func listNameVersion(dir string) []string {
 
 			} else {
 				//print(ss);
-				split := strings.Split(ss, dir)
+				split := strings.Split(ss, config.CellarPath)
 				splitLen := len(split)
 				if (splitLen == 2) {
 					found = append(found, split[1])
