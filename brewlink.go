aeee8f1bf94f75b8975b90d64efd67a94aeb53bd
Martin Page <wookoouk@gmail.com>
2016-03-31 11:17:20 +0100

    test

--- brewlink.go
+++ brewlink.go
@@ -91,7 +91,7 @@ func listNameVersion(dir string) []string {
 					splitLen := len(split)
 
 					for _, s := range split {
-						println(versionFolder,"TO",ss)
+						println(versionFolder,"TO",s)
 					}
 
 					//println("len", splitLen)
